21a01dd79fdf2f37c2a835d5fd8a01fcc225481a
harshitsharma-dev <harshitsharma182021@gmail.com>
2025-06-03 14:32:38 +0530

    port

--- app.py
+++ app.py
@@ -500,4 +500,4 @@ def apply_cors(response):
     return response
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
