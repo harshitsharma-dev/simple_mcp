7f08adc8c80b6297812250be716bdec1cec72cce
harshitsharma-dev <harshitsharma182021@gmail.com>
2025-06-03 16:13:20 +0530

    port

--- app.py
+++ app.py
@@ -500,4 +500,5 @@ def apply_cors(response):
     return response
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
